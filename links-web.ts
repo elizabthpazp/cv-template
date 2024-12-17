ea2f2e0c7814d18fd022f19324f7d4d3b35385a3
elizabthpazp <elizabthpazp695@gmail.com>
2024-12-17 14:45:34 -0600

    feat: change username in links

--- links-web.ts
+++ links-web.ts
@@ -3,15 +3,15 @@ export const links = {
     github: "https://github.com/elizabthpazp",
     web: "https://elijs.dev",
     githubBlog: "https://github.com/elizabthpazp/blog",
-    username: "elizabthpazp",
+    username: "elijs.dev",
     telegram: "https://t.me/elizabthpazp",
-    instagram: "https://instagram.com/elizabthpazp.js",
-    tiktok: "https://www.tiktok.com/@elizabthpazp.js",
+    instagram: "https://instagram.com/elijs.dev",
+    tiktok: "https://www.tiktok.com/@elijs.dev",
     linkedin: "https://www.linkedin.com/in/elizabeth-de-la-paz-portal-4b864b209",
     discord: "https://discord.com/users/1062024233577893918",
     facebook: "https://m.facebook.com/100006840368095/", 
-    twitter: "https://twitter.com/elizabthpazp",  
-    threads: "https://www.threads.net/@elizabthpazp.js",
+    twitter: "https://twitter.com/elijs_dev",  
+    threads: "https://www.threads.net/@elijs.dev",
     youtube: "https://www.youtube.com/@elijsdev"
 } as const
  
